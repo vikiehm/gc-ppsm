ead7d792a27c3fb2b468079522eecade547b75c4
Viktoria Ehm <ehm@atcremers97.vision.in.tum.de>
2025-07-30 13:55:34 +0200

    adjust typo

--- matlab/call_main.m
+++ matlab/call_main.m
@@ -1,7 +1,7 @@
 curr_animal = "cat";
 % TODO add shape1, shape2 name and data folder
 shape_1 = "cat-7";
-shape_2 = "dog-6";
+shape_2 = "cat-6";
 data_folder = "/usr/wiss/ehm/Development/gc-ppsm/low_res_meshes/";
 curr_lambda = "0";
 interval_search(curr_animal, shape_1, shape_2, curr_lambda, data_folder)
